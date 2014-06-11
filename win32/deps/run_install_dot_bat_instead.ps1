b1e9361250b2bb5cc745e05da81d839e36ba9ab6
hansdude <hansk@spectralogic.com>
2014-06-11 10:55:01 -0600

    Removed iconv support from libxml2 because we don't actually need to do any character set conversions.

--- win32/deps/run_install_dot_bat_instead.ps1
+++ win32/deps/run_install_dot_bat_instead.ps1
@@ -41,21 +41,10 @@ cd ..\..\..
 copy "$curl\builds\libcurl-vc-x86-release-static-zlib-static-ipv6-sspi-spnego-winssl\lib\libcurl_a.lib" "$installFolder\lib"
 xcopy /s "$curl\builds\libcurl-vc-x86-release-static-zlib-static-ipv6-sspi-spnego-winssl\include" "$installFolder\include"
 
-# Compile and install iconv.
-# Using 1.9.2 because later versions don't support nmake+msvc.
-# This isn't ideal. We should probably look at using exporting a
-# mingw dll to msvc but this works for now.
-$iconv = "build\libiconv-1.9.2"
-cd $iconv
-nmake -f Makefile.msvc NO_NLS=1 MFLAGS=-MD
-cd ..\..
-copy "$iconv\lib\iconv.lib" "$installFolder\lib"
-copy "$iconv\include\iconv.h" "$installFolder\include"
-
 # Compile and install libxml.
 $libxml2 = "build\libxml2-2.9.1"
 cd "$libxml2\win32"
-cscript configure.js include="$installFolder\include" lib="$installFolder\lib"
+cscript configure.js include="$installFolder\include" lib="$installFolder\lib" iconv=no
 nmake -f Makefile.msvc
 cd ..\..\..
 copy "$libxml2\win32\bin.msvc\libxml2_a.lib" "$installFolder\lib"
