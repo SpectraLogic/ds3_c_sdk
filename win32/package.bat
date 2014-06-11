557d5ed7c65bf83ce7af771e24c48d9d56f2a910
hansdude <hansk@spectralogic.com>
2014-06-10 18:55:45 -0600

    Added a package bat file.

---
+++ win32/package.bat
@@ -0,0 +1,5 @@
+@echo off
+set VISUAL_STUDIO_INSTALL_PATH=C:\Program Files (x86)\Microsoft Visual Studio 9.0
+call "%VISUAL_STUDIO_INSTALL_PATH%\VC\vcvarsall.bat" x86
+powershell -ExecutionPolicy RemoteSigned -File run_package_dot_bat_instead.ps1
+
