a2f3d731dc8df4f96b831209c882c375691e5693
Klopsch <samuelklopsch@gmail.com>
2015-07-23 14:09:09 -0600

    Finished up first search test

--- test/search_tests.cpp
+++ test/search_tests.cpp
@@ -5,20 +5,24 @@
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE( get_objects ) {
+    uint64_t num_objs;
     ds3_client* client = get_client();
     ds3_get_objects_response* response;
     const char* bucket_name = "search_bucket_test";
     populate_with_objects(client, bucket_name);
 
     ds3_request* request = ds3_init_get_objects(bucket_name);
-    _set_query_param(request, "bucket_id", bucket_name);
     ds3_request_set_name(request, "%txt%");
 
     ds3_error* error = ds3_get_objects(client, request, &response);
 
     handle_error(error);
+    num_objs = response->num_objects;
+	
+    BOOST_CHECK_EQUAL(num_objs, 5);
 
     ds3_free_request(request);
-    ds3_free_service_response(response);
+    ds3_free_objects_response(response);
+    clear_bucket(client, bucket_name);
     free_client(client);
 }
