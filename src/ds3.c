6bae2ad112845a404e447db55d036f2d74cb046d
Ryan Moore <ryan.moore@greyrocksoft.com>
2015-08-06 23:11:09 +0000

    Implemented the get_object_with_metadata call

--- src/ds3.c
+++ src/ds3.c
@@ -1717,8 +1717,21 @@ ds3_error* ds3_get_object(const ds3_client* client, const ds3_request* request, 
     return _internal_request_dispatcher(client, request, user_data, callback, NULL, NULL);
 }
 
-ds3_error* ds3_get_object_with_metadata(const ds3_client* client, const ds3_request* request, void* user_data, size_t (* callback)(void*, size_t, size_t, void*), ds3_metadata** metadata) {
-    return _internal_request_dispatcher(client, request, NULL, NULL, NULL, NULL);
+ds3_error* ds3_get_object_with_metadata(const ds3_client* client, const ds3_request* request, void* user_data, size_t (* callback)(void*, size_t, size_t, void*), ds3_metadata** _metadata) {
+  ds3_error* error;
+    GHashTable* return_headers;
+    ds3_metadata* metadata;
+
+    error = _net_process_request(client, request, user_data, callback, NULL, NULL, &return_headers);
+
+    if (error == NULL) {
+        fprintf(stderr, "Head object completed successfully\n");
+        metadata = _init_metadata(return_headers);
+        *_metadata = metadata;
+        g_hash_table_destroy(return_headers);
+    }
+
+    return error;
 }
 
 ds3_error* ds3_put_object(const ds3_client* client, const ds3_request* request, void* user_data, size_t (*callback)(void*, size_t, size_t, void*)) {
