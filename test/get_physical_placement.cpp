b86c6126e8bb5fc9fbe3fa37242853fef5464290
DenverM80 <denverm@spectralogic.com>
2015-08-31 14:42:13 -0600

    modify xml_get_string() to deal with empty XML elements

--- test/get_physical_placement.cpp
+++ test/get_physical_placement.cpp
@@ -23,17 +23,14 @@ BOOST_AUTO_TEST_CASE( get_physical_placment ){
     uint64_t num_tapes;
 
     ds3_client* client = get_client();
-    //const char* bucket_name = "unit_test_bucket";
-    const char* bucket_name = "Spectra_BlackPearl_Backup";
-
+    const char* bucket_name = "unit_test_bucket";
     populate_with_objects(client, bucket_name);
 
     ds3_bulk_object_list* object_list = ds3_init_bulk_object_list(1);
 
     ds3_bulk_object obj;
     memset(&obj, 0, sizeof(ds3_bulk_object));
-    //obj.name = ds3_str_init("resources/not-beowulf.txt");
-    obj.name = ds3_str_init("full_backup_2015-08-24_03-00-00.tar.gz");
+    obj.name = ds3_str_init("resources/not-beowulf.txt");
     object_list->list[0] = obj;
 
     request = ds3_init_get_physical_placement(bucket_name, object_list);
@@ -50,10 +47,65 @@ BOOST_AUTO_TEST_CASE( get_physical_placment ){
     }
 
     num_tapes = get_physical_placement_response->num_tapes;
-    printf("Found %lu tapes.", num_tapes);
     BOOST_CHECK_EQUAL(num_tapes, 0);
 
     ds3_free_get_physical_placement_response(get_physical_placement_response);
     clear_bucket(client, bucket_name);
     free_client(client);
 }
+
+// For this test to actually work, we need an object to be already written to tape.
+/*
+BOOST_AUTO_TEST_CASE( get_physical_placment_full_details ){
+
+    printf("-----Testing GetPhysicalPlacement with full_details -------\n");
+
+
+    ds3_request* request = NULL;
+    ds3_error* error = NULL;
+    ds3_get_physical_placement_response* get_physical_placement_response = NULL;
+    uint64_t num_tapes;
+
+    ds3_client* client = get_client();
+    const char* bucket_name = "test_bucket";
+
+    ds3_bulk_object_list* object_list = ds3_init_bulk_object_list(1);
+
+    ds3_bulk_object obj;
+    memset(&obj, 0, sizeof(ds3_bulk_object));
+    obj.name = ds3_str_init("ideaIC-14.1.4.exe");
+    object_list->list[0] = obj;
+
+    request = ds3_init_get_physical_placement(bucket_name, object_list);
+    error = ds3_get_physical_placement(client, request, &get_physical_placement_response);
+
+    ds3_free_request(request);
+    ds3_free_bulk_object_list(object_list);
+    handle_error(error);
+    if (get_physical_placement_response == NULL) {
+        BOOST_MESSAGE("The response was null without an error, which means we have no tape backend.  Skipping the test.");
+        clear_bucket(client, bucket_name);
+        ds3_free_client(client);
+        return;
+    }
+
+    num_tapes = get_physical_placement_response->num_tapes;
+    printf("\n\nFound %lu tapes.\n\n", num_tapes);
+    BOOST_CHECK(num_tapes > 0);
+
+    BOOST_CHECK_EQUAL(get_physical_placement_response->tapes[0].assigned_to_bucket, True);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].available_raw_capacity != NULL);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].barcode != NULL);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].bucket_id != NULL);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].id != NULL);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].last_accessed != NULL);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].last_checkpoint != NULL);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].last_modified != NULL);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].partition_id != NULL);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].serial_number != NULL);
+    BOOST_CHECK(get_physical_placement_response->tapes[0].total_raw_capacity != NULL);
+
+    ds3_free_get_physical_placement_response(get_physical_placement_response);
+    free_client(client);
+}
+*/
