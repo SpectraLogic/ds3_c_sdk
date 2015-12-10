91d380279a6820ed7792419e458917aefbdc4e37
berkeleysquare <johnk@spectralogic.com>
2015-12-10 11:47:10 -0700

    Copyright Headers

--- sample/samples.h
+++ sample/samples.h
@@ -1,8 +1,20 @@
-/*** Shared header for all Samples  ***/
-/*** Spectra Logic December 9, 2015 ***/
+/*
+ * ******************************************************************************
+ *   Copyright 2014-2015 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
 
 /*
- * Set environmet vars to match target device, e.g.:
+ * Set environment vars to match target device, e.g.:
  	export DS3_ACCESS_KEY="ams="
 	export DS3_SECRET_KEY="tzcWTTYE"
 	export DS3_ENDPOINT="http://192.168.1.20:8080"
