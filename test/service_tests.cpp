2c498231f170bd1903d168bc3165ddd80ecf35c4
Charles Hanchett <charlesh@spectralogic.com>
2016-01-07 16:40:22 -0700

    worked on finding bug in service test

--- test/service_tests.cpp
+++ test/service_tests.cpp
@@ -9,7 +9,6 @@ BOOST_AUTO_TEST_CASE( get_service ) {
     ds3_get_service_response* response;
 
     printf("-----Testing GET service-------\n");
-    
     ds3_error* error = ds3_get_service(client, request, &response);
 
     BOOST_CHECK(error == NULL);
