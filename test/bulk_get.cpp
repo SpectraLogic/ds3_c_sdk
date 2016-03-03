ea8cb3d8e689767f7322f522ad3e3bf72e141c79
Charles Hanchett <charlesh@spectralogic.com>
2016-03-03 12:29:32 -0700

    fixed error in checksum with passing 0 for number of bytes to check, change checksum to only use glib functions

--- test/bulk_get.cpp
+++ test/bulk_get.cpp
@@ -398,26 +398,35 @@ BOOST_AUTO_TEST_CASE( escape_urls ) {
     const char *delimiters[4] = {"or", "/", "@", "="};
     const char *strings_to_test[5] = {"some normal text", "/an/object/name", "bytes=0-255,300-400,550-800", "orqwerty/qwerty@qwerty=", "`1234567890-=~!@#$%^&*()_+[]\\{}|;:,./<>?"};
     const char *object_name_results[5] = {"some%20normal%20text", "/an/object/name", "bytes%3D0-255%2C300-400%2C550-800", "orqwerty/qwerty%40qwerty%3D",
-                                        "%601234567890-%3D~%21%40%23%24%25%5E%26%2A%28%29_%2B%5B%5D%7B%7D%7C%3B%3A%2C./%3C%3E%3F"};
+                                        "%601234567890-%3D~%21%40%23%24%25%5E%26%2A%28%29_%2B%5B%5D%5C%7B%7D%7C%3B%3A%2C./%3C%3E%3F"};
     const char *range_header_results[5] = {"some%20normal%20text", "%2Fan%2Fobject%2Fname", "bytes=0-255,300-400,550-800", "orqwerty%2Fqwerty%40qwerty=",
-                                         "%601234567890-=~%21%40%23%24%25%5E%26%2A%28%29_%2B%5B%5D%7B%7D%7C%3B%3A,.%2F%3C%3E%3F"};
+                                         "%601234567890-=~%21%40%23%24%25%5E%26%2A%28%29_%2B%5B%5D%5C%7B%7D%7C%3B%3A,.%2F%3C%3E%3F"};
     const char *general_delimiter_results[5] = {"some%20normal%20text", "/an/object/name", "bytes=0-255%2C300-400%2C550-800", "orqwerty/qwerty@qwerty=",
-                                              "%601234567890-=~%21@%23%24%25%5E%26%2A%28%29_%2B%5B%5D%7B%7D%7C%3B%3A%2C./%3C%3E%3F"};
+                                              "%601234567890-=~%21@%23%24%25%5E%26%2A%28%29_%2B%5B%5D%5C%7B%7D%7C%3B%3A%2C./%3C%3E%3F"};
 
     printf("-----Testing escape url helpers-------\n");
 
     for (int i = 0; i < 5; i++) {
         char* escaped_url = escape_url_object_name(strings_to_test[i]);
+	if(strcmp(escaped_url, object_name_results[i]) != 0){
+	  printf("%s != %s\n", escaped_url, object_name_results[i]);
+	}
         BOOST_CHECK(strcmp(escaped_url, object_name_results[i]) == 0);
         free(escaped_url);
     }
     for (int i = 0; i < 5; i++) {
         char* escaped_url = escape_url_range_header(strings_to_test[i]);
+	if(strcmp(escaped_url, range_header_results[i]) != 0){
+	  printf("%s != %s\n", escaped_url, range_header_results[i]);
+	}
         BOOST_CHECK(strcmp(escaped_url, range_header_results[i]) == 0);
         free(escaped_url);
     }
     for (int i = 0; i < 5; i++) {
         char* escaped_url = escape_url_extended(strings_to_test[i], delimiters, 4);
+	if(strcmp(escaped_url, general_delimiter_results[i]) != 0){
+	  printf("%s != %s\n", escaped_url, general_delimiter_results[i]);
+	}
         BOOST_CHECK(strcmp(escaped_url, general_delimiter_results[i]) == 0);
         free(escaped_url);
     }
