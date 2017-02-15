b52cb4e437c3bcdea6980f7e6a9c856d1db256f4
Yuriy Shestakov <yuriy.shestakov@archive-engines.com>
2017-02-15 17:32:58 +0200

    fixed %install steps

--- buildrpm.sh
+++ buildrpm.sh
@@ -7,5 +7,5 @@ sver=${ver#v}
 # git archive --format=tar --prefix=ds3_c_sdk-${sver}/ ${ver}^{tree} |\
 git archive --format=tar --prefix=ds3_c_sdk-${sver}-$rev-$id/ HEAD |\
     gzip > ~/rpmbuild/SOURCES/ds3_c_sdk-${sver}.tar.gz
-sed -e 's/%{sdk_version}/'$sver'/' < ds3_c_sdk.spec.in > ds3_c_sdk.spec
-rpmbuild -D "sdk_version ${sver}" -D "sdk_rev ${rev}" -D "sdk_id ${id}" -ba ds3_c_sdk.spec
+sed -e 's/%{sdk_version}/'$sver'/; s/%{_sdk_rev}/'${rev}'/; s/%{_sdk_id}/'${id}'/' < ds3_c_sdk.spec.in > ds3_c_sdk.spec
+rpmbuild -D "sdk_version ${sver}" -D "_sdk_rev ${rev}" -D "_sdk_id ${id}" -ba ds3_c_sdk.spec
