71cf34bcecbd899967d06d3d5435d81aded42b34
DenverM80 <denverm@spectralogic.com>
2017-04-03 12:39:16 -0600

    Print the git rev hash when running tests in the docker container

--- docker/run_mem_tests.sh
+++ docker/run_mem_tests.sh
@@ -6,38 +6,26 @@ echo DS3_ENDPOINT ${DS3_ENDPOINT}
 echo DS3_SECRET_KEY ${DS3_SECRET_KEY}
 echo DS3_ACCESS_KEY ${DS3_ACCESS_KEY}
 
-echo "cd /opt"
+set -x
+
 cd /opt
 
 if [ ${GIT_BRANCH} != "master" ]; then
-  echo git clone ${GIT_REPO} --branch ${GIT_BRANCH} --single-branch
   git clone ${GIT_REPO} --branch ${GIT_BRANCH} --single-branch
 else
-  echo git clone ${GIT_REPO}
   git clone ${GIT_REPO}
 fi
 
-echo "cd ds3_c_sdk"
 cd ds3_c_sdk
+git rev-parse HEAD
 
-echo "cmake ."
 cmake .
-
-echo "make"
 make
-
-echo "make install"
 make install
 ldconfig
 
-echo "cd test"
 cd test
-
-echo "cmake ."
 cmake .
-
-echo "make"
 make
-echo "make mem"
 make mem
 
